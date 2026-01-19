cbed02fce99aec19ba4642ddeeda323fdc8e81d8
Sumit Gupta <sg855591@gmail.com>
2026-01-19 23:50:21 +0530

    Finished Project

---
+++ app/api/order/seller-orders/route.js
@@ -0,0 +1,29 @@
+import connectDB from "@/config/db";
+import authSeller from "@/lib/authSeller";
+import Address from "@/models/Address";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+
+export async function GET(req) {
+  try {
+    const { userId } = getAuth(req);
+
+    const isSeller = await authSeller(userId)
+
+    if(!isSeller) {
+      return NextResponse.json({ success: false, message: 'Unauthorized access'});
+    }
+
+    await connectDB();
+
+    Address.length
+
+    const orders = await Order.find({}).populate('address items.product');
+
+    return NextResponse.json({ success: true, orders})
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message});
+  }
+}
--- models/Order.js
+++ models/Order.js
@@ -4,17 +4,17 @@ const OrderSchema = new mongoose.Schema({
   userId: {
     type: String, 
     required: true,
-    ref: 'user'
+    ref: 'User'
   },
   items: [{
     product: {
-      type: String, required: true, ref: 'product'
+      type: String, required: true, ref: 'Product'
     },
     quantity: { type: Number, required: true },
     
   }],
   amount: { type: Number, required: true },
-  address: { type: String, required: true, ref: 'address' },
+  address: { type: String, required: true, ref: 'Address' },
   status: { type: String, default: 'Order Placed' }, 
   date: { type: Number, default: Date.now, required: true },
 
